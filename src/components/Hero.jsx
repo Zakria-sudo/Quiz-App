ac178793be9dd5e171092be775b3d31e90308795
Zaki <gostudyzaki@gmail.com>
2025-06-19 00:18:21 +0500

    TypeWriter Effect on Welcome-Page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { Typewriter } from "react-simple-typewriter";
 
 const Hero = () => {
   return (
-    
-      <div className="container flex flex-col justify-center items-center content-center h-screen">
-        <h1 className="font-bold text-4xl">Welcome to the Quiz App</h1>
-        <div className="btn my-6">
-            <button className="bg-yellow-600 hover:bg-yellow-500 text-white font-semibold px-6 py-3 rounded-full shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 active:scale-95 focus:outline-none focus:ring-4 focus:ring-yellow-300">Continue</button>
-        </div>
+    <div className="container flex flex-col justify-center items-center content-center h-screen">
+      <h1 className="font-bold text-4xl text-center my-6 text-white">
+        <Typewriter
+          words={["Welcome to the Quiz App"]}
+          loop={1}
+          cursor
+          cursorStyle=""
+          typeSpeed={60}
+          delaySpeed={1000}
+        />
+      </h1>
+      <div className="btn my-6">
+        <button className="bg-yellow-600 hover:bg-yellow-500 text-white font-semibold px-6 py-3 rounded-full shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 active:scale-95 focus:outline-none focus:ring-4 focus:ring-yellow-300">
+          Continue
+        </button>
       </div>
-    
+    </div>
   );
 };
 
